034fcf5260429d1152116fc6eefe04f4f9899ff2
Rachel <rachel.black@flashtalking.com>
2016-10-01 16:22:15 +0100

    added creature type

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,15 +8,15 @@ export function hideModal(){
     return { type : actionTypes.HIDE_MODAL}
 }
 
-
 export function createNewCreature(){
-
-    function Creature(name, age, mana ) {
-        this.name = name
+    const creatureTypes = ['phoenixes', 'medusas', 'hippogriffs']
+    function Creature(name, age, mana, type ) {
+        this.name = ""
         this.age = Math.random() * (501 - 0) + 0
         this.mana = Math.random() * (10001 - 1000) + 1000
+        this.type = creatureTypes[Math.round(Math.round((Math.random() * 2)))]
     }
-    const newCreature = new Creature("test")
+    const newCreature = new Creature("")
 
     return {type: actionTypes.CREATE_NEW_CREATURE, creature : newCreature}
 }
