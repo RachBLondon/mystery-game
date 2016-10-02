976d5a70cd007fbea8214eb22bebf6a210539b84
Rachel <rachel.black@flashtalking.com>
2016-10-02 12:04:44 +0100

    view reducer tests added

--- test/reducers/view.test.js
+++ test/reducers/view.test.js
@@ -10,4 +10,22 @@ describe('React View Reducer', function () {
         const state = reducer({}, action)
         expect(state).to.eql({ displayModal: true })
     })
+
+    it('HIDE_MODAL', () => {
+        const action = { type: actions.HIDE_MODAL }
+        const state = reducer({}, action)
+        expect(state).to.eql({ displayModal: false })
+    })
+
+    it('NOTIFY_ERROR', ()=>{
+        const action = { type: actions.NOTIFY_ERROR}
+        const state = reducer({}, action)
+        expect(state).to.eql({nameError :true})
+    })
+
+    it('removes error on ADD_NAME_TO_NEW_CREATURE', ()=> {
+        const action  = {type: actions.ADD_NAME_TO_NEW_CREATURE}
+        const state = reducer({}, action)
+        expect(state).to.eql({nameError: false})
+    })
 })
