6303cf0984d06da055140e9e4a51d09351dbd368
Rachel <rachel.black@flashtalking.com>
2016-10-02 12:54:09 +0100

    app component test

---
+++ test/components/app.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {expect} from 'chai'
+import {shallow, mount, render} from 'enzyme'
+import App from '../../src/components/app'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducers from './../../src/reducers'
+
+function configureStore(initalState) {
+    const createStoreWithMiddleware = createStore(
+        rootReducers,
+        initalState,
+        compose(
+            applyMiddleware())
+    )
+    return createStoreWithMiddleware
+}
+const store = configureStore({})
+
+
+describe('<App />', ()=> {
+
+    it('displays a h1', ()=>{
+        const wrapper = render(
+            <Provider store={store} >
+                <App/>
+            </Provider>    )
+        expect(wrapper.html()).to.contain('h1')
+    })
+})
