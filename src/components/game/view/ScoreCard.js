c7085d0f308ff79beb959379f9200a5ca575eac7
Rachel <rachel.black@flashtalking.com>
2016-10-09 19:04:25 +0100

    floating cloud animation

--- src/components/game/view/ScoreCard.js
+++ src/components/game/view/ScoreCard.js
@@ -13,13 +13,17 @@ export default class ScoreCard extends Component {
 
     render(){
         return (
-            <div className="c-score-cloud">
-                <div className="c-score-card">
-                    <p>Total Age : {this.tally(this.props.creatures, 'age')}</p>
-                    <p>Total Mana : {this.tally(this.props.creatures, 'mana')}</p>
-                    <p>Number of Cages available: {this.props.creatures.length} / 10</p>
+            <div className="c-score-card-container">
+                <div className="c-score-card-container-animate">
+                    <div className="c-score-cloud">
+                        <div className="c-score-card">
+                            <p>Total Age : {this.tally(this.props.creatures, 'age')}</p>
+                            <p>Total Mana : {this.tally(this.props.creatures, 'mana')}</p>
+                            <p>Number of Cages available: {this.props.creatures.length} / 10</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+             </div>
         )
     }
 }
