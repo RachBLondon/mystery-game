eeb7df6e07ba5a1e0a9a87883f0d4c943a999b92
Rachel <rachel.black@flashtalking.com>
2016-10-10 09:58:57 +0100

    tally func switched from map to reduce

--- src/components/game/view/ScoreCard.js
+++ src/components/game/view/ScoreCard.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react'
 
 export default class ScoreCard extends Component {
     tally(array, key){
-        let score = 0;
-        array.map(function(item){
-            console.log(item[key])
-            score = score + item[key]
-        })
-        return score
+        return array.reduce(function(acc, obj){
+            return acc + obj[key]
+        },0)
     }
 
 
